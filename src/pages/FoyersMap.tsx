acbaa2b964ad94e294aedc70853deeb5cef8359e
kastrupf <kastrupf@gmail.com>
2020-10-17 19:38:22 +0200

    inclusion du map de Bordeaux dans la page de map de foyers

--- src/pages/FoyersMap.tsx
+++ src/pages/FoyersMap.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/foyers-map.css';
+import { url } from 'inspector';
 
 function FoyersMap() {
   return (
@@ -24,7 +28,13 @@ function FoyersMap() {
 
 			</aside>
 
-			<div></div>
+			<Map
+				center={[44.8584692,-0.5716759]}
+				zoom={15}
+				style={{ width: '100%', height: '100%'}}
+			>	
+				<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+			</Map> 
 
 			<Link to="" className="create-foyer">
 				<FiPlus size={32} color="#FFF" />
